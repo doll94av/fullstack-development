4e2adb0dd48c251634a5c2a1c2156d417b090360
doll94av <45698606+doll94av@users.noreply.github.com>
2021-07-18 11:10:18 -0400

    Updated frontend and backend code
    Found a way to store the data from the backend within the state of the frontend. After doing this I changed the backend response to mimic what we are expecting  in the frontend

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,20 +4,46 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from ya mama"});
-});
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+  res.json({ message: "test: /"});
+});
+
 app.get("/home", (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  res.json({ message: "Hello from ya mama"});
+  res.json({
+    generalText: "we clicked the home button and we got this info from the backend",
+    projectText: "On the home page we will probably just talk about work experience and the project",
+    codeText: "Home page this may be blank unless I can find something relevant to fill the space with"
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json({
+    generalText: "Thank you for your interest, I am looking forward to hearing from you!",
+    projectText: "<linkedIn, email>",
+    codeText: "probs empty"
+  });
+});
+
+app.get("/webscraper", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json({
+    generalText: "we clicked the webscraper button and we got this info from the backend",
+    projectText: "I used go and a package called 'colly' to create a simple webscraper. This takes in some reddit link and will scrap the first 10 unique images that it sees. Once it has scraped the relevant images it will store the contents into a .zip and offer it for download!",
+    codeText: "Probably a few snippets regarding what I did"
+  });
+});
+
+app.get("/mongobackend", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json({
+    generalText: "we clicked the mongobackend button and we got this info from the backend",
+    projectText: "Backend code / database code / whatever",
+    codeText: "more code that does not exist yet"
+  });
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+
 class Buttons extends React.Component {
   constructor(props) {
     super(props)
@@ -23,7 +24,6 @@ class Buttons extends React.Component {
 
 
 class BodyFeild extends React.Component {
-
   //default text
   state = {
     generalText: 'Welcome to my portfolio! I put together this small site to demonstrate some of my knowledge as well as grow my skillset. This site is built using MERN as a stack, when I started this project I had little frontend and backend knowlege. Spending time here has helped teach me how different components link together as well as how to manage a fullstack project from start to finish (though this is ever growing). Please feel free to check it out, any advice or comments are always welcome! All of my contact details will be available on the contact page.',
@@ -37,17 +37,25 @@ class BodyFeild extends React.Component {
     var projectText = ''
     var codeText = ''
 
-    //test logic
     //currently this works with no spaces (might want to add logic to trim spaces if I really care about it)
-
+    //Strucutre in the backend mimics what we have in the frontend so sending data back and fourth is clear
     fetch('http://localhost:3001/'+ event.target.name)
       .then(response => response.json())
-      .then(data => console.log(data));
-    this.setState({
-      generalText: generalText,
-      projectText: projectText,
-      codeText: codeText
-    });
+      .then(data =>
+        this.setState({
+          generalText: data.generalText,
+          projectText: data.projectText,
+          codeText: data.codeText
+        })
+      );
+
+    // keeping this for now just to be safe
+    // this.setState({
+    //   generalText: generalText,
+    //   projectText: projectText,
+    //   codeText: codeText
+    // });
+
 
     //this is the button that was clicked
     console.log(event.target.name)
